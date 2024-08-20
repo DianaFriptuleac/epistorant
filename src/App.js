f36f27c4fc64c8627355e4ebabb10d89b3aab978
Diana <dianadorojuc3@gmail.com>
2024-08-20 10:00:45 +0200

    first commit bootstrap react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css' //devo includere in app.js il file css di bootstrap presente in node_modules
+//bootstrap.min.css- css di bootstrap piu leggero e compatto.
+import './App.css'; //il mio css e meglio importarlo dopo bootstrap
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-success border border-danger'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
